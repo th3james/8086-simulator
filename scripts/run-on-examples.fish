85245399eb44dac5d0cf9fb777d212f19178abed
James Cox-Morton <james.cox-morton@arm.com>
2024-05-08 20:59:45 +0100

    Nicer run output

--- scripts/run-on-examples.fish
+++ scripts/run-on-examples.fish
@@ -4,6 +4,11 @@ zig build; and begin
   for binary_file in test-data/*.bin
       set source_file (string replace ".bin" ".asm" $binary_file)
 
-      diff (./zig-out/bin/8086-simulator $binary_file | psub) $source_file
+      echo "diffing output for" $source_file
+      diff (
+        ./zig-out/bin/8086-simulator $binary_file | grep -vE '^\s*;|^\s*$' | psub
+      ) (
+        cat $source_file | grep -vE '^\s*;|^\s*$' | psub
+      )
   end
 end

733ad59cd69e33b196350d45885951a472f44f2f
James Cox-Morton <james.cox-morton@arm.com>
2024-06-17 16:47:20 +0100

    Use enum to represent registersu

--- src/decode/register_names.zig
+++ src/decode/register_names.zig
@@ -1,19 +1,21 @@
 const std = @import("std");
 const bit_utils = @import("bit_utils.zig");
 
-const RegisterName = struct {
-    narrow: []const u8,
-    wide: []const u8,
+const Register = enum { al, cl, dl, bl, ah, ch, dh, bh, ax, cx, dx, bx, sp, bp, si, di, none };
+
+const RegisterInfo = struct {
+    narrow: Register,
+    wide: Register,
 };
-const registerMap = [_]RegisterName{
-    RegisterName{ .narrow = "al", .wide = "ax" },
-    RegisterName{ .narrow = "cl", .wide = "cx" },
-    RegisterName{ .narrow = "dl", .wide = "dx" },
-    RegisterName{ .narrow = "bl", .wide = "bx" },
-    RegisterName{ .narrow = "ah", .wide = "sp" },
-    RegisterName{ .narrow = "ch", .wide = "bp" },
-    RegisterName{ .narrow = "dh", .wide = "si" },
-    RegisterName{ .narrow = "bh", .wide = "di" },
+const registerMap = [_]RegisterInfo{
+    RegisterInfo{ .narrow = Register.al, .wide = Register.ax },
+    RegisterInfo{ .narrow = Register.cl, .wide = Register.cx },
+    RegisterInfo{ .narrow = Register.dl, .wide = Register.dx },
+    RegisterInfo{ .narrow = Register.bl, .wide = Register.bx },
+    RegisterInfo{ .narrow = Register.ah, .wide = Register.sp },
+    RegisterInfo{ .narrow = Register.ch, .wide = Register.bp },
+    RegisterInfo{ .narrow = Register.dh, .wide = Register.si },
+    RegisterInfo{ .narrow = Register.bh, .wide = Register.di },
 };
 
 pub fn registerName(reg: u8, wide: bool) []const u8 {
@@ -22,7 +24,7 @@ pub fn registerName(reg: u8, wide: bool) []const u8 {
     }
 
     const names = registerMap[reg];
-    return if (wide) names.wide else names.narrow;
+    return @tagName(if (wide) names.wide else names.narrow);
 }
 
 test "registerName options" {
@@ -30,18 +32,18 @@ test "registerName options" {
     try std.testing.expectEqualStrings("cx", registerName(0b001, true));
 }
 
-const effectiveAddressRegisterMap = [_][2][2]u8{
-    [2][2]u8{ .{ 'b', 'x' }, .{ 's', 'i' } },
-    [2][2]u8{ .{ 'b', 'x' }, .{ 'd', 'i' } },
-    [2][2]u8{ .{ 'b', 'p' }, .{ 's', 'i' } },
-    [2][2]u8{ .{ 'b', 'p' }, .{ 'd', 'i' } },
-    [2][2]u8{ .{ 's', 'i' }, .{ '0', '0' } },
-    [2][2]u8{ .{ 'd', 'i' }, .{ '0', '0' } },
-    [2][2]u8{ .{ 'b', 'p' }, .{ '0', '0' } },
-    [2][2]u8{ .{ 'b', 'x' }, .{ '0', '0' } },
+const effectiveAddressRegisterMap = [_][2]Register{
+    .{ Register.bx, Register.si },
+    .{ Register.bx, Register.di },
+    .{ Register.bp, Register.si },
+    .{ Register.bp, Register.di },
+    .{ Register.si, Register.none },
+    .{ Register.di, Register.none },
+    .{ Register.bp, Register.none },
+    .{ Register.bx, Register.none },
 };
 
-const EffectiveAddress = struct { r1: [2]u8, r2: [2]u8, displacement: u16 };
+const EffectiveAddress = struct { r1: Register, r2: Register, displacement: u16 };
 // Table 4-10. R/M (Register/Memory) Field Encoding
 pub fn effectiveAddressRegisters(regOrMem: u3, mod: u2, displacement: [2]u8) EffectiveAddress {
     var offset: u16 = 0;
@@ -64,22 +66,22 @@ pub fn effectiveAddressRegisters(regOrMem: u3, mod: u2, displacement: [2]u8) Eff
 
 test "effective address options no displacement" {
     const result = effectiveAddressRegisters(0b000, 0b00, [_]u8{ 0b0, 0b0 });
-    try std.testing.expectEqualStrings("bx", &result.r1);
-    try std.testing.expectEqualStrings("si", &result.r2);
+    try std.testing.expectEqual(Register.bx, result.r1);
+    try std.testing.expectEqual(Register.si, result.r2);
     try std.testing.expectEqual(0, result.displacement);
 }
 
 test "effective address options 8-bit displacement" {
     const result = effectiveAddressRegisters(0b000, 0b01, [_]u8{ 0b1, 0b0 });
-    try std.testing.expectEqualStrings("bx", &result.r1);
-    try std.testing.expectEqualStrings("si", &result.r2);
+    try std.testing.expectEqual(Register.bx, result.r1);
+    try std.testing.expectEqual(Register.si, result.r2);
     try std.testing.expectEqual(1, result.displacement);
 }
 
 test "effective address options 16-bit displacement" {
     const result = effectiveAddressRegisters(0b000, 0b10, [_]u8{ 0b10, 0b1 });
-    try std.testing.expectEqualStrings("bx", &result.r1);
-    try std.testing.expectEqualStrings("si", &result.r2);
+    try std.testing.expectEqual(Register.bx, result.r1);
+    try std.testing.expectEqual(Register.si, result.r2);
     try std.testing.expectEqual(258, result.displacement);
 }
 
@@ -87,9 +89,9 @@ pub fn renderEffectiveAddress(effectiveAddress: EffectiveAddress, allocator: std
     var args = std.ArrayList([]const u8).init(allocator);
     defer args.deinit();
 
-    try args.append(try allocator.dupe(u8, &effectiveAddress.r1));
-    if ((effectiveAddress.r2[0] != '0') and (effectiveAddress.r2[0] != '0')) {
-        try args.append(try allocator.dupe(u8, &effectiveAddress.r2));
+    try args.append(@tagName(effectiveAddress.r1));
+    if (effectiveAddress.r2 != Register.none) {
+        try args.append(@tagName(effectiveAddress.r2));
     }
 
     if (effectiveAddress.displacement != 0) {
@@ -104,21 +106,21 @@ pub fn renderEffectiveAddress(effectiveAddress: EffectiveAddress, allocator: std
 }
 
 test "render effective address no displacement" {
-    const input = EffectiveAddress{ .r1 = [2]u8{ 'b', 'x' }, .r2 = [2]u8{ 's', 'i' }, .displacement = 0 };
+    const input = EffectiveAddress{ .r1 = Register.bx, .r2 = Register.si, .displacement = 0 };
     const result = try renderEffectiveAddress(input, std.heap.page_allocator);
     defer std.heap.page_allocator.free(result);
     try std.testing.expectEqualStrings("[bx + si]", result);
 }
 
 test "render effective address ignore zero register" {
-    const input = EffectiveAddress{ .r1 = [2]u8{ 's', 'i' }, .r2 = [2]u8{ '0', '0' }, .displacement = 0 };
+    const input = EffectiveAddress{ .r1 = Register.si, .r2 = Register.none, .displacement = 0 };
     const result = try renderEffectiveAddress(input, std.heap.page_allocator);
     defer std.heap.page_allocator.free(result);
     try std.testing.expectEqualStrings("[si]", result);
 }
 
 test "render effective address with displacement" {
-    const input = EffectiveAddress{ .r1 = [2]u8{ 'b', 'x' }, .r2 = [2]u8{ 's', 'i' }, .displacement = 250 };
+    const input = EffectiveAddress{ .r1 = Register.bx, .r2 = Register.si, .displacement = 250 };
     const result = try renderEffectiveAddress(input, std.heap.page_allocator);
     defer std.heap.page_allocator.free(result);
     try std.testing.expectEqualStrings("[bx + si + 250]", result);

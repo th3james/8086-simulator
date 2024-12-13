9682513954e93dee2d46f3c34e1a8417faa70eb2
James Cox-Morton <james.cox-morton@arm.com>
2024-12-13 16:57:29 +0000

    Add emulator test

--- scripts/run-on-examples.fish
+++ scripts/run-on-examples.fish
@@ -2,13 +2,23 @@
 
 zig build; and begin
   for binary_file in test-data/*.bin
-      set source_file (string replace ".bin" ".asm" $binary_file)
+    set source_file (string replace ".bin" ".asm" $binary_file)
+    set emulation_output (string replace ".bin" ".txt" $binary_file)
 
-      echo "diffing output for" $source_file
+    echo "diffing output for" $source_file
+    diff (
+      ./zig-out/bin/8086-simulator $binary_file | grep -vE '^\s*;|^\s*$' | psub
+    ) (
+      cat $source_file | grep -vE '^\s*;|^\s*$' | psub
+    )
+
+    if test -f $emulation_output
+      echo "comparing emulation output for" $emulation_output
       diff (
-        ./zig-out/bin/8086-simulator $binary_file | grep -vE '^\s*;|^\s*$' | psub
+        ./zig-out/bin/8086-simulator --execute $binary_file | grep -vE '^\s*$' | psub
       ) (
-        cat $source_file | grep -vE '^\s*;|^\s*$' | psub
+        cat $emulation_output | grep -vE '^\s*;|^\s*$' | psub
       )
+    end
   end
 end

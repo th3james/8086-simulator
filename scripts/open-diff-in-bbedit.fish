becd59ea157a6607ec674db52c258b8b33a1af2e
James Cox-Morton <james.cox-morton@arm.com>
2025-02-20 14:10:59 +0000

    Show decode and execution descrepancies

--- scripts/open-diff-in-bbedit.fish
+++ scripts/open-diff-in-bbedit.fish
@@ -2,8 +2,24 @@
 
 zig build; and begin
   set binary_file $argv[1]
+  set source_file_name (basename $binary_file)
 
+  set source_file (string replace ".bin" ".asm" $binary_file)
   set emulation_output (string replace ".bin" ".txt" $binary_file)
 
-  bbedit (diff (zig-out/bin/8086-simulator --execute $binary_file | psub) $emulation_output | psub)
+  bbedit (
+    diff (
+      ./zig-out/bin/8086-simulator $binary_file | grep -vE '^\s*;|^\s*$' | psub
+    ) (
+      cat $source_file | grep -vE '^\s*;|^\s*$' | psub
+    ) | psub -s $source_file_name.asm
+  )
+
+  bbedit (
+    diff (
+      ./zig-out/bin/8086-simulator --execute $binary_file | psub
+    ) (
+      cat $emulation_output | psub
+    ) | psub -s $source_file_name.txt
+  )
 end
